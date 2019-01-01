afd83b0b8d63a16d0c40ac19fefc961ab82e3b80
Olav Schettler <olav@schettler.net>
2019-01-01 18:49:29 +0100

    Added missing files

---
+++ routes.php
@@ -0,0 +1,29 @@
+<?php
+
+use Knowfox\Passwordless\Controllers\LoginController;
+use Knowfox\Passwordless\Controllers\RegisterController;
+
+Route::group(['middleware' => 'web'], function () {
+    Route::get('login', LoginController::class . '@showLoginForm')
+        ->name('login');
+
+    Route::post('login', LoginController::class . '@login');
+
+    Route::get('register', RegisterController::class . '@showRegistrationForm')->name('register');
+    Route::post('register', RegisterController::class . '@register');
+
+    Route::get('cancel/{what}/{email}', [
+        'as' => 'cancel',
+        'uses' => RegisterController::class . '@cancel',
+    ]);
+
+    Route::get('auth/email-authenticate/{token}', [
+        'as' => 'auth.email-authenticate',
+        'uses' => LoginController::class . '@authenticateEmail',
+    ]);
+
+    Route::post('logout', LoginController::class . '@logout')
+        ->name('logout');
+
+});
+
