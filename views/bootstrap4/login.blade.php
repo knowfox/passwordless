1c03bee915ea89969783d58c06aa7542b33d956a
Olav Schettler <olav@schettler.net>
2019-01-16 18:08:52 +0100

    Added missing <row>

--- views/bootstrap4/login.blade.php
+++ views/bootstrap4/login.blade.php
@@ -22,7 +22,7 @@
                                     <p><strong>@lang('Please enter your email address to log into :app.', ['app' => config('app.name')])</strong></p>
                                 @endif
 
-                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
                                     <label for="email" class="col-md-4 control-label">@lang('E-Mail Address')</label>
 
                                     <div class="col-md-6">
@@ -36,7 +36,7 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group">
+                                <div class="form-group row">
                                     <div class="col-md-8 col-md-offset-4">
                                         <button type="submit" class="btn btn-primary">
                                             @lang('Login')
